ab235f97b9eab062388ecf59c7a9bcfaf90b5517
Lucia <lucia@wcw-staff-145-109-91-233.wireless.uva.nl>
2025-11-10 16:51:27 +0100

    Add test cases

--- tests/test_models.py
+++ tests/test_models.py
@@ -4,8 +4,50 @@ import numpy as np
 import numpy.testing as npt
 import pytest
 
-from inflammation.models import daily_mean, daily_max, daily_min
+from inflammation.models import daily_mean, daily_max, daily_min, patient_normalise
 
+
+@pytest.mark.parametrize(
+    "test, expected, expect_raises",
+    [
+        (
+            [[-1, 2, 3], [4, 5, 6], [7, 8, 9]],
+            None,
+            ValueError('inflammation values should be non-negative'),
+        ),
+        (
+            [4, 5, 6],
+            None,
+            ValueError('inflammation array should be 2-dimensional'),
+        ),
+        (
+            'hello',
+            None,
+            TypeError('data input should be ndarray'),
+        ),
+        (
+            3,
+            None,
+            TypeError('data input should be ndarray'),
+        ),
+        (
+            [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+            [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+            None,
+        )
+    ])
+
+def test_patient_normalise(test, expected, expect_raises):
+    """Test normalisation works for arrays of one and positive integers."""
+    if isinstance(test, list):
+        test = np.array(test)
+    if expect_raises is not None:
+        with pytest.raises(expect_raises, match=str(expect_raises)):
+          patient_normalise(test)
+
+    else:
+        result = patient_normalise(test)
+        npt.assert_allclose(result, np.array(expected), rtol=1e-2, atol=1e-2)
 
 def test_daily_mean_zeros():
     """Test that mean function works for an array of zeros."""
