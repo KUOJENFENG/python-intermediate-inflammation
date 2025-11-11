c35658cba996a3069c6063e945be541cc14f8129
Lucia <lucia@Lucias-MacBook-Pro.local>
2025-11-11 10:36:37 +0100

    Resolved merge conflicts in requirements.txt and test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -4,7 +4,50 @@ import numpy as np
 import numpy.testing as npt
 import pytest
 
-from inflammation.models import daily_mean, daily_max, daily_min
+from inflammation.models import daily_mean, daily_max, daily_min, patient_normalise
+
+
+@pytest.mark.parametrize(
+    "test, expected, expect_raises",
+    [
+        (
+            [[-1, 2, 3], [4, 5, 6], [7, 8, 9]],
+            None,
+            ValueError('inflammation values should be non-negative'),
+        ),
+        (
+            [4, 5, 6],
+            None,
+            ValueError('inflammation array should be 2-dimensional'),
+        ),
+        (
+            'hello',
+            None,
+            TypeError('data input should be ndarray'),
+        ),
+        (
+            3,
+            None,
+            TypeError('data input should be ndarray'),
+        ),
+        (
+            [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+            [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+            None,
+        )
+    ])
+
+def test_patient_normalise(test, expected, expect_raises):
+    """Test normalisation works for arrays of one and positive integers."""
+    if isinstance(test, list):
+        test = np.array(test)
+    if expect_raises is not None:
+        with pytest.raises(expect_raises, match=str(expect_raises)):
+          patient_normalise(test)
+
+    else:
+        result = patient_normalise(test)
+        npt.assert_allclose(result, np.array(expected), rtol=1e-2, atol=1e-2)
 
 def test_daily_mean_zeros():
     """Test that mean function works for an array of zeros."""
@@ -18,7 +61,6 @@ def test_daily_mean_zeros():
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
-
 def test_daily_mean_integers():
     """Test that mean function works for an array of positive integers."""
 
@@ -30,17 +72,19 @@ def test_daily_mean_integers():
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
-def test_daily_max():
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([ [0, 0, 0], [0, 0, 0], [0, 0, 0] ], [0, 0, 0]),
+        ([ [4, 2, 5], [1, 6, 2], [4, 1, 9] ], [4, 6, 9]),
+        ([ [4, -2, 5], [1, -6, 2], [-4, -1, 9] ], [4, -1, 9]),
+    ])
+
+def test_daily_max(test, expected):
     """Test that maximum function works for an array of zeros."""
     
-
-    test_input = np.array([[1, 4],
-                           [2, 9],
-                           [100, 10]])
-    test_result = np.array([100, 10])
-
     # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_max(test_input), test_result)
+    npt.assert_array_equal(daily_max(np.array(test)), np.array(expected))
 
 def test_daily_mean():
     """Test that mean function works."""
@@ -54,21 +98,22 @@ def test_daily_mean():
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
-def test_daily_min():
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([ [0, 0, 0], [0, 0, 0], [0, 0, 0] ], [0, 0, 0]),
+        ([ [4, 2, 5], [1, 6, 2], [4, 1, 9] ], [1, 1, 2]),
+        ([ [4, -2, 5], [1, -6, 2], [-4, -1, 9] ], [-4, -6, 2]),
+    ])
+
+def test_daily_min(test, expected):
     """Test that minimum function works for an array of zeros."""
     
-
-    test_input = np.array([[-1, 4],
-                           [12, 79],
-                           [10, 11]])
-    test_result = np.array([-1, 4])
-
     # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_min(test_input), test_result)
+    npt.assert_array_equal(daily_min(np.array(test)), np.array(expected))
 
 def test_daily_min_string():
     """Test for TypeError when passing strings"""
 
     with pytest.raises(TypeError):
         error_expected = daily_min([['Hello', 'there'], ['General', 'Kenobi']])
-
