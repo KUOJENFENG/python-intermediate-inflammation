3c73e4d951bb0336fc08d75d756829d3a66008b7
Lucia <lucia@Lucias-MacBook-Pro.local>
2025-11-25 10:41:46 +0100

    Refactoring - test compute_data.py

--- inflammation-analysis.py
+++ inflammation-analysis.py
@@ -4,8 +4,8 @@
 import argparse
 import os
 
-from inflammation import models, views
-from inflammation.compute_data import analyse_data
+from inflammation import models, views, compute_data
+from inflammation.compute_data import JSONDataSource, CSVDataSource, analyse_data
 
 
 def main(args):
@@ -15,25 +15,43 @@ def main(args):
     - selecting the necessary models and views for the current task
     - passing data between models and views
     """
-    infiles = args.infiles
-    if not isinstance(infiles, list):
-        infiles = [args.infiles]
+    in_files = args.infiles
+    if not isinstance(in_files, list):
+        in_files = [args.infiles]
 
+    for filename in in_files:
+        _, extension = os.path.splitext(filename)
+        if extension == '.json':
+            data_source = compute_data.JSONDataSource(os.path.dirname(filename))
+        elif extension == '.csv':
+            data_source = compute_data.CSVDataSource(os.path.dirname(filename))
+        else:
+            raise ValueError(f'Unsupported data file format: {extension}')
+        compute_data.analyse_data(data_source)
 
-    if args.full_data_analysis:
-        analyse_data(os.path.dirname(infiles[0]))
-        return
-
-    for filename in infiles:
+    for filename in in_files:
         inflammation_data = models.load_csv(filename)
 
-        view_data = {
-            'average': models.daily_mean(inflammation_data),
-            'max': models.daily_max(inflammation_data),
-            'min': models.daily_min(inflammation_data)
-        }
+        view_data = {'average': models.daily_mean(inflammation_data), 
+                     'max': models.daily_max(inflammation_data), 
+                     'min': models.daily_min(inflammation_data)}
 
         views.visualize(view_data)
+    
+    if args.full_data_analysis:
+        _, extension = os.path.splitext(in_files[0])
+        if extension == '.json':
+            data_source = JSONDataSource(os.path.dirname(in_files[0]))
+        elif extension == '.csv':
+            data_source = CSVDataSource(os.path.dirname(in_files[0]))
+        else:
+            raise ValueError(f'Unsupported file format: {extension}')
+        data_result = analyse_data(data_source)
+        graph_data = {
+            'standard deviation by day': data_result,
+        }
+        views.visualize(graph_data)
+        return
 
 
 if __name__ == "__main__":
@@ -44,12 +62,13 @@ if __name__ == "__main__":
         'infiles',
         nargs='+',
         help='Input CSV(s) containing inflammation series for each patient')
-
+    
     parser.add_argument(
-        '--full-data-analysis',
+        '--full_data_analysis',
         action='store_true',
-        dest='full_data_analysis')
+        help='Perform a full data analysis, including standard deviation by day visualization')
 
     args = parser.parse_args()
 
     main(args)
+
